5741e3694d0d3c94fe80314158a9ae1f8d107d03
Dennis Holmer <dennis@zwapgrid.com>
2020-06-04 14:59:19 +0000

    Fix AuthorizationConnector [168536825]

---
+++ FortnoxAPILibrary.Tests/AuthorizationTests.cs
@@ -0,0 +1,30 @@
+﻿using FortnoxAPILibrary.Connectors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FortnoxAPILibrary.Tests
+{
+    [TestClass]
+    public class AuthorizationTests : ConnectorTestsBase
+    {
+        private readonly AuthorizationConnector _connector;
+
+        public AuthorizationTests()
+        {
+            _connector = new AuthorizationConnector();
+        }
+
+        [TestMethod]
+        [Ignore("Needs a new authorization code each time")]
+        public void GetAccessToken()
+        {
+            //Set auth code here to test
+            const string authorizationCode = "";
+
+            var accessToken = _connector.GetAccessToken(authorizationCode, ClientSecret);
+
+            MyAssert.HasNoError(_connector);
+            
+            Assert.IsNotNull(accessToken);
+        }
+    }
+}
